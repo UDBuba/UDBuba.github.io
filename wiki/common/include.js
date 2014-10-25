b2fc0db1e40b3b36888c4f4a6269784e666d5172
UDBuba <bubaatudomain@gmail.com>
2014-10-25 13:36:45 +0800

    build

--- wiki/common/include.js
+++ wiki/common/include.js
@@ -1,6 +1,6 @@
 function loadScript(url)
 {
-    var head = document.getElementsByTagName('head');
+    var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
